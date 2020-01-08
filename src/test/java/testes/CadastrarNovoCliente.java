c804c902827f6fab6dfd8d1bdfb1135fe5170e65
George <georgeluiz.ti@gmail.com>
2020-01-08 12:15:17 -0300

    Projeto criado com seu caso de teste DeveCadastrarNovoClienteComSucesso

---
+++ src/test/java/testes/CadastrarNovoCliente.java
@@ -0,0 +1,43 @@
+package testes;
+
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CadastrarNovoCliente {
+	private WebDriver driver;
+	@Before
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get("https://www.advantageonlineshopping.com/");
+	}
+	
+	@Test
+	public void DeveCadastrarNovoClienteComSucesso() {
+		boolean displayed_hiUser = false;
+		try {
+			 displayed_hiUser = driver.findElement(By.xpath("//span[@class='hi-user containMiniTitle ng-binding']")).isDisplayed();
+		} catch (Exception e) {
+			assertTrue(displayed_hiUser);
+		}
+		
+		
+	}
+	
+	
+	@After
+	public void end() throws Exception {
+		Thread.sleep(3000);
+		driver.quit();
+	}
+}
